651ce160411fe34fb68cbe45890a8659eca6b0ae
Paolo Fernandez <fernandez.pafr@gmail.com>
2015-03-27 14:24:01 -0400

    con algunos cambios

--- app/assets/javascripts/cpu_binaries.js
+++ app/assets/javascripts/cpu_binaries.js
@@ -8,3 +8,13 @@
 	$('#cpu_binary_store').regexMask(/^[01]+$/);
 	$('#cpu_binary_add').regexMask(/^[01]+$/);
 	$('#cpu_binary_sub').regexMask(/^[01]+$/);
+
+	$("#binary_input").keydown(function(e) {
+    console.log(e.keyCode);
+    if(e.keyCode === 49 || e.keyCode === 48 || e.keyCode === 8 ){
+        console.log("binario");
+    } else {
+        e.preventDefault();
+        console.log("bloqueado");
+    }      
+});
