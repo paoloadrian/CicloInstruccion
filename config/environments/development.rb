dc7008933910d7d3a603c599d3eec752cdd049bf
Paolo Adrian Fernandez Roman <piro_pafr@hotmail.com>
2015-05-10 23:54:58 -0400

    con bug q no dejaba deployar arreglado

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.assets.precompile += [/.*\.js/,/.*\.css/]
+  
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -21,7 +23,7 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
+  
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
