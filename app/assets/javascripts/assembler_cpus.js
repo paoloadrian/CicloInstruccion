f998d02ff50337ba12538d385f4ec080662e7371
Paolo Adrian Fernandez Roman <piro_pafr@hotmail.com>
2015-05-10 23:30:00 -0400

    con registros de uso general funcionando

--- app/assets/javascripts/assembler_cpus.js
+++ app/assets/javascripts/assembler_cpus.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
 	$("#assembler_cpu_pc").regexMask(/^[0-9]+$/);
-	$("#assembler_cpu_architecture_1").change(function () {
-		console.log(this.val());
-		if(this.val() == 2)
-			$("#directions").hide();
-		else
-			$("#directions").show();
-	});
 });
