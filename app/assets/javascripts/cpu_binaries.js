094b73883246de082486e1bcbcacc8bef4b835da
Paolo Adrian Fernandez Roman <piro_pafr@hotmail.com>
2015-05-26 00:15:29 -0400

    con arreglos de interfaz terminados

--- app/assets/javascripts/cpu_binaries.js
+++ app/assets/javascripts/cpu_binaries.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var tamCO=0, tamDir=0, tam=0;
 	$("#cpu_binary_co").regexMask(/^\d+$/);
 	$("#cpu_binary_dir").regexMask(/^\d+$/);
 	$("#cpu_binary_pc").regexMask(/^[01]+$/);
@@ -12,23 +13,48 @@ $(document).ready(function(){
 		$("#cpu_binary_sub").attr({maxlength: this.value});
 		$("#cpu_binary_store").attr({maxlength: this.value});
 		$("#cpu_binary_load").attr({maxlength: this.value});
+		if (this.value != ""){
+			tamCO = parseInt(this.value);
+			if(tamCO==0)
+				tamCO=0;
+		}
+		else
+			tamCO = 0;
 
 		if ($("#cpu_binary_dir").val() != ""){
-			var tam = parseInt(this.value) + parseInt($("#cpu_binary_dir").val());
+			tam = tamCO + tamDir;
 			$("#cpu_binary_pc").attr({maxlength: tam.toString()});
+			$("#c").text(tamCO);
 		}
 		else{
+			tam = tamCO;
+			$("#c").text(tamCO-1);
 			$("#cpu_binary_pc").attr({maxlength: this.value});
 		}
+		$("#b").text(tamCO-1);
+		$("#d").text(tam-1);
 	});
 
 	$("#cpu_binary_dir").change(function(){
+		if (this.value != ""){
+			tamDir = parseInt(this.value);
+			if(tamDir==0)
+				tamDir=0;
+		}
+		else
+			tamDir = 0;
+
 		if ($("#cpu_binary_co").val() != ""){
-			var tam = parseInt(this.value) + parseInt($("#cpu_binary_co").val());
+			tam = tamCO + tamDir;
+			$("#c").text(tamCO);
 			$("#cpu_binary_pc").attr({maxlength: tam.toString()});
 		}
 		else{
+			tam = tamDir;
+			$("#c").text(0);
 			$("#cpu_binary_pc").attr({maxlength: this.value});
 		}
+		$("#b").text(tamCO-1);
+		$("#d").text(tam-1);
 	});
 });
--- app/assets/javascripts/hexa_cpus.js
+++ app/assets/javascripts/hexa_cpus.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var tamCO=0, tamDir=0, tam=0;
 	$("#hexa_cpu_co").regexMask(/^\d+$/);
 	$("#hexa_cpu_dir").regexMask(/^\d+$/);
 	$("#hexa_cpu_pc").regexMask(/^[0-9A-Fa-f]+$/);
@@ -12,29 +13,48 @@ $(document).ready(function(){
 		$("#hexa_cpu_sub").attr({maxlength: this.value});
 		$("#hexa_cpu_store").attr({maxlength: this.value});
 		$("#hexa_cpu_load").attr({maxlength: this.value});
+		if (this.value != ""){
+			tamCO = parseInt(this.value)*4;
+			if(tamCO==0)
+				tamCO=0;
+		}
+		else
+			tamCO = 0;
 
 		if ($("#hexa_cpu_dir").val() != ""){
-			var tam = parseInt(this.value) + parseInt($("#hexa_cpu_dir").val());
+			tam = tamCO + tamDir;
 			$("#hexa_cpu_pc").attr({maxlength: tam.toString()});
+			$("#c").text(tamCO);
 		}
 		else{
+			tam = tamCO;
+			$("#c").text(tamCO-1);
 			$("#hexa_cpu_pc").attr({maxlength: this.value});
 		}
+		$("#b").text(tamCO-1);
+		$("#d").text(tam-1);
 	});
 
 	$("#hexa_cpu_dir").change(function(){
+		if (this.value != ""){
+			tamDir = parseInt(this.value)*4;
+			if(tamDir==0)
+				tamDir=0;
+		}
+		else
+			tamDir = 0;
+
 		if ($("#hexa_cpu_co").val() != ""){
-			var tam = parseInt(this.value) + parseInt($("#hexa_cpu_co").val());
+			tam = tamCO + tamDir;
+			$("#c").text(tamCO);
 			$("#hexa_cpu_pc").attr({maxlength: tam.toString()});
 		}
 		else{
+			tam = tamDir;
+			$("#c").text(0);
 			$("#hexa_cpu_pc").attr({maxlength: this.value});
 		}
-	});
-	$("#hexa_cods input[type='text']").keyup(function(){
-    	this.value = this.value.toUpperCase();
-	});
-	$("#hexa_cpu_pc").keyup(function(){
-    	this.value = this.value.toUpperCase();
+		$("#b").text(tamCO-1);
+		$("#d").text(tam-1);
 	});
 });
