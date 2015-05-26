b688d02dee01685519ee4b1b9e0b01845c604d56
Paolo Adrian Fernandez Roman <piro_pafr@hotmail.com>
2015-05-26 01:14:14 -0400

    con arreglos de interfaz terminados

--- app/assets/javascripts/assembler_cpus.js
+++ app/assets/javascripts/assembler_cpus.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
 	$("#assembler_cpu_pc").regexMask(/^[0-9]+$/);
+	$("#assembler_cpu_architecture_1").change(function(){
+		if($(this).is(':checked'))
+			$("#directions").show();
+	});
+	$("#assembler_cpu_architecture_2").change(function(){
+		if($(this).is(':checked'))
+			$("#directions").hide();
+	});
 });
