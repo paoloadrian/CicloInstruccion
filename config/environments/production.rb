796f8e798b3b57be53cfd45e273868d4ddab823d
Paolo Adrian Fernandez Roman <piro_pafr@hotmail.com>
2015-08-24 06:35:25 -0400

    mejora de interfaz

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,19 +34,19 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
- #  config.action_mailer.delivery_method = :smtp
- #  config.action_mailer.default_url_options = { :host => 'https://ciclodeinstruccion.herokuapp.com' }
- #  config.action_mailer.perform_deliveries = true
- #  config.action_mailer.default :charset => "utf-8"
- #  config.action_mailer.smtp_settings = {
-	# :address => 'smtp.gmail.com',
-	# :port => 587,
-	# :authentication => 'plain',
-	# :user_name => 'ciclodeinstruccion@gmail.com',
-	# :password => 'cihi.2015',
-	# :enable_starttls_auto => true,
-	# :domain => 'https://ciclodeinstruccion.herokuapp.com'
- #  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'https://ciclodeinstruccion.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+	:address => 'smtp.gmail.com',
+	:port => 587,
+	:authentication => 'plain',
+	:user_name => 'ciclodeinstruccion@gmail.com',
+	:password => 'cihi.2015',
+	:enable_starttls_auto => true,
+	:domain => 'https://ciclodeinstruccion.herokuapp.com'
+  }
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
